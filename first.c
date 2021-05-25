03180b2f01d18bfba330e0c8d449115e0e433f24
João Costa <joaocosta_neto@hotmail.com>
2021-05-25 12:48:26 -0300

    Update first.c
    help the code

--- first.c
+++ first.c
@@ -12,17 +12,17 @@ void exibirTabuleiro(){
 void lancarJogada(){
     int contador = 0;
     if(tabuleiro[posicao] != 'X' && tabuleiro[posicao] != 'O' ){
-    tabuleiro[posicao] = player;
-    if(player == 'X'){
-        player = 'O';
+        tabuleiro[posicao] = player;
+        switch (player) {
+            case "X":
+                player = "O";
+            break;
+            case "O":
+                player = "X";
+            break;
+        }
         contador++;
         exibirTabuleiro();
-    }else{
-        
-        player = 'X';
-        contador++;
-        exibirTabuleiro();
-    }
     }
 }
 
@@ -31,58 +31,22 @@ void lancarJogada(){
 void jogada(){
     printf("Selecione a posicao que deseja marcar:\n");
     scanf("%d", &posicao);
-    switch (posicao)
-    {
-    case 1:
-    lancarJogada();
-       break;
-
-       case 2:
-       lancarJogada();
-       break;
-    case 3 :
-    lancarJogada();
-    break;
-    case 4 :
-    lancarJogada();
-    break;
-
-    case 5:
-    lancarJogada();
-    break;
-
-    case 6:
-    lancarJogada();
-    break;
-
-    case 7:
-    lancarJogada();
-    break;
-
-    case 8:
-    lancarJogada();
-    break;
-
-    case 9:
-    lancarJogada();
-    break;
-    default:
-    printf("O numero %d é inválido, selecione um valor entre 1 e 9 que não esteja em uso\n", posicao);
-    lancarJogada();
-        break;
+    if(posicao >= 1 && posicao <= 0) {
+        lancarJogada()
+    } else {
+        printf("O numero %d é inválido, selecione um valor entre 1 e 9 que não esteja em uso\n", posicao);
     }
 }
 /*Valida a jogada e compara com as possibilidades de vencer, caso seja encontrada alguma declara vencedor. */
 void validar(){
-char vetorTabuleiro[25] = {1,2,3,4,5,6,7,8,9,1,4,7,2,5,8, 3,7,9,1,5,9, 3,5,7};// lista as possibilidades de formar linha ( que sao 8 ).
-
-for(int i=0; i<8; i++)
-if((tabuleiro[vetorTabuleiro[i]] == 'X' && tabuleiro[vetorTabuleiro[i]+1] == 'X' && tabuleiro[vetorTabuleiro[i+2]] == 'X') || (tabuleiro[vetorTabuleiro[i]] == 'O' || tabuleiro[vetorTabuleiro[i]+1] == 'O' || tabuleiro[vetorTabuleiro[i]+2] == 'O')){
-    printf("Parabens voce ganhou!.");
-}else{
-    jogada();
-}
-
+    char vetorTabuleiro[25] = {1,2,3,4,5,6,7,8,9,1,4,7,2,5,8, 3,7,9,1,5,9, 3,5,7};// lista as possibilidades de formar linha ( que sao 8 ).
+    for(int i=0; i<8; i++) {
+        if((tabuleiro[vetorTabuleiro[i]] == 'X' && tabuleiro[vetorTabuleiro[i]+1] == 'X' && tabuleiro[vetorTabuleiro[i+2]] == 'X') || (tabuleiro[vetorTabuleiro[i]] == 'O' || tabuleiro[vetorTabuleiro[i]+1] == 'O' || tabuleiro[vetorTabuleiro[i]+2] == 'O')){
+            printf("Parabens voce ganhou!.");
+        }else{
+            jogada();
+        }
+    }
 }
 
 int main (void)
@@ -93,4 +57,3 @@ int main (void)
 
 
 }
-
