84c0ecd5c104bb9a1a85daa96e9e80150759ae49
FelipexCodex <77489249+FelipexCodex@users.noreply.github.com>
2021-05-26 20:19:51 -0300

    Update first.c

--- first.c
+++ first.c
@@ -1,96 +1,108 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
+
+/* Variaveis globais, constantes, arrays e outros*/
+
 char player = 'X';
-char tabuleiro[11] = {'0','1','2','3','4','5','6','7','8','9'} ;
+char tabuleiro[11] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 int posicao;
+
 /* Exibe o Tabuleiro atualizado a cada Jogada */
-void exibirTabuleiro(){
-    printf("|++++|++++|++++|\n|  %c |  %c |  %c |\n|++++|++++|++++|\n|  %c |  %c |  %c |\n|++++|++++|++++|\n|  %c |  %c |  %c |\n|++++|++++|++++|\n", tabuleiro[1],tabuleiro[2],tabuleiro[3],tabuleiro[4],tabuleiro[5],tabuleiro[6],tabuleiro[7],tabuleiro[8],tabuleiro[9]);
-
+void exibirTabuleiro()
+{
+    printf("|++++|++++|++++|\n|  %c |  %c |  %c |\n|++++|++++|++++|\n|  %c |  %c |  %c |\n|++++|++++|++++|\n|  %c |  %c |  %c |\n|++++|++++|++++|\n", tabuleiro[1], tabuleiro[2], tabuleiro[3], tabuleiro[4], tabuleiro[5], tabuleiro[6], tabuleiro[7], tabuleiro[8], tabuleiro[9]);
 }
+
 /* Insere o Caracter do jogador atual no campo do tabuleiro desde que o mesmo esteja livre. */
-void lancarJogada(){
+void lancarJogada()
+{
     int contador = 0;
-    if(tabuleiro[posicao] != 'X' && tabuleiro[posicao] != 'O' ){
-    tabuleiro[posicao] = player;
-    if(player == 'X'){
-        player = 'O';
-        contador++;
-        exibirTabuleiro();
-    }else{
-        
-        player = 'X';
-        contador++;
-        exibirTabuleiro();
-    }
+    if (tabuleiro[posicao] != 'X' && tabuleiro[posicao] != 'O')
+    {
+        tabuleiro[posicao] = player;
+        if (player == 'X')
+        {
+            player = 'O';
+            contador++;
+            exibirTabuleiro();
+        }
+        else
+        {
+            player = 'X';
+            contador++;
+            exibirTabuleiro();
+        }
     }
 }
-
 
 /* Inicia a jogada validando se o campo esta livre para inserir o caracter do respectivo jogador. */
-void jogada(){
+void jogada()
+{
     printf("Selecione a posicao que deseja marcar:\n");
     scanf("%d", &posicao);
     switch (posicao)
     {
     case 1:
-    lancarJogada();
-       break;
+        lancarJogada();
+        break;
 
-       case 2:
-       lancarJogada();
-       break;
-    case 3 :
-    lancarJogada();
-    break;
-    case 4 :
-    lancarJogada();
-    break;
+    case 2:
+        lancarJogada();
+        break;
+    case 3:
+        lancarJogada();
+        break;
+    case 4:
+        lancarJogada();
+        break;
 
     case 5:
-    lancarJogada();
-    break;
+        lancarJogada();
+        break;
 
     case 6:
-    lancarJogada();
-    break;
+        lancarJogada();
+        break;
 
     case 7:
-    lancarJogada();
-    break;
+        lancarJogada();
+        break;
 
     case 8:
-    lancarJogada();
-    break;
+        lancarJogada();
+        break;
 
     case 9:
-    lancarJogada();
-    break;
+        lancarJogada();
+        break;
     default:
-    printf("O numero %d é inválido, selecione um valor entre 1 e 9 que não esteja em uso\n", posicao);
-    lancarJogada();
+        printf("O numero %d é inválido, selecione um valor entre 1 e 9 que não esteja em uso.\n"\, posicao);
+        lancarJogada();
         break;
     }
 }
+
+
 /*Valida a jogada e compara com as possibilidades de vencer, caso seja encontrada alguma declara vencedor. */
-void validar(){
-char vetorTabuleiro[25] = {1,2,3,4,5,6,7,8,9,1,4,7,2,5,8, 3,7,9,1,5,9, 3,5,7};// lista as possibilidades de formar linha ( que sao 8 ).
+void varificarGanhador()
+{
+    /* lista as possibilidades de formar linha ( que sao 8 ). */
+    char vetorTabuleiro[25] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 4, 7, 2, 5, 8, 3, 7, 9, 1, 5, 9, 3, 5, 7}; 
 
-for(int i=0; i<8; i++)
-if((tabuleiro[vetorTabuleiro[i]] == 'X' && tabuleiro[vetorTabuleiro[i]+1] == 'X' && tabuleiro[vetorTabuleiro[i+2]] == 'X') || (tabuleiro[vetorTabuleiro[i]] == 'O' || tabuleiro[vetorTabuleiro[i]+1] == 'O' || tabuleiro[vetorTabuleiro[i]+2] == 'O')){
-    printf("Parabens voce ganhou!.");
-}else{
-    jogada();
+    for (int i = 0; i < 8; i++)
+        if ((tabuleiro[vetorTabuleiro[i]] == 'X' && tabuleiro[vetorTabuleiro[i] + 1] == 'X' && tabuleiro[vetorTabuleiro[i + 2]] == 'X') || (tabuleiro[vetorTabuleiro[i]] == 'O' || tabuleiro[vetorTabuleiro[i] + 1] == 'O' || tabuleiro[vetorTabuleiro[i] + 2] == 'O'))
+        {
+            printf("Parabens voce ganhou!.");
+        }
+        else
+        {
+            jogada();
+        }
 }
 
-}
-
-int main (void)
+int main(void)
 {
 
-  exibirTabuleiro();
+    exibirTabuleiro();
     jogada();
-
-
 }
-
